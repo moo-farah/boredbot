12dd67826f800e384bb61688b428ec0645d16781
moo-farah <rizmik.farah@gmail.com>
2026-08-10 23:32:03 +0300

    add: the css styling

--- Boredbot/index.js
+++ Boredbot/index.js
@@ -1,6 +1,13 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
- .then(response => response.json())
- .then(data => {
-    console.log(data)
-    document.getElementById('activity').textContent =  data.activity
- })
+const activityEl = document.getElementById('activity')
+const btnEl = document.getElementById('get-activity-btn')
+
+function getActivity() {
+   fetch("https://apis.scrimba.com/bored/api/activity")
+   .then(response => response.json())
+   .then(data => {
+      console.log(data)
+      document.getElementById('activity').textContent =  data.activity
+   })
+}
+
+btnEl.addEventListener('click', getActivity)
