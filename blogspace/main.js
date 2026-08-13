552f362c81dd92dea2d9235943fdf088302d7089
moo-farah <rizmik.farah@gmail.com>
2026-08-13 16:36:32 +0300

    fix: added the form feature

--- blogspace/main.js
+++ blogspace/main.js
@@ -7,6 +7,7 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(data => {
         // slice to 5
         const itemList = data.slice(0, 5)
+        console.log(itemList);
 
         //  display the `title` and `body` properties of the first 5 posts on the browser page.
 
@@ -14,9 +15,30 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 
         // looping and build the HTML string
         itemList.forEach(post => {
-            emptyArr += `<h2>${post.title}</h2>
-            <p>${post.body}</p>`
+            emptyArr += 
+            `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            `
         });
 
         document.getElementById("posts").innerHTML = emptyArr;
+    })
+
+    // form handling
+
+    const form = document.getElementById('post-form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const title = document.getElementById('post-title').value;
+        const body = document.getElementById('post-body').value;
+
+        const post = {
+            title,
+            body
+        };
+
+        console.log(post);
     })
